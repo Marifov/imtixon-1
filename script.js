c7bec1d3f05624f018cfa1a2af9870ff4870bdda
Marifov <147223007+Marifov@users.noreply.github.com>
2023-10-07 08:43:01 +0500

    imtixon

---
+++ script.js
@@ -0,0 +1,16 @@
+const block34 = document.querySelector(".block3-4")
+
+
+function ochish1() {
+    if (block34.style.visibility==='hidden') {
+        block34.style.visibility = 'visible'
+        block34.style.margin= '30px 0px 200px 0px'
+    }
+
+    else{
+        block34.style.visibility ='hidden'
+        block34.style.margin= '0px 0px  0px 0px'
+    }
+}
+
+
